a19b93e4f7aae2c736d4c394ef6d61d40dce91a6
Jay <panchalj272@gmail.com>
2023-01-05 19:12:29 +0530

    Feature: Removed Error from about Section

--- components/about/About.js
+++ components/about/About.js
@@ -26,7 +26,7 @@ const About = () => {
           <a className="button buttonFlex">
             Downlaod CV
             <svg
-              class="buttonIcon"
+              className="buttonIcon"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
