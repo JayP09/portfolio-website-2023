07cee2588f26e3354ed36b0f0543177b337c73c8
Jay <panchalj272@gmail.com>
2023-01-10 23:18:37 +0530

    Feature: Added Meta tags for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Jay Panchal • Software Engineer </title>
+        <meta name="description" content="Portfolio Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+        <meta property="og:url" content="https://jaypanchal.me" />
+        <meta property="og:title" content="Jay Panchal • Software Developer" />
+        <meta property="og:site_name" content="Jay Panchal" />
+        <meta property="og:type" content="website" />
+        <link rel="canonical" href="" />
+        <meta property="og:image" content="" />
       </Head>
       <body>
         <Main />
