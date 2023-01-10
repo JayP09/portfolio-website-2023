219ae627870285d68a8dbb70aeba11487384bdfa
Jay <panchalj272@gmail.com>
2023-01-10 23:17:18 +0530

    Feature: Added Loading Screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/globals.css'
+import React, {useState, useEffect} from "react"
+import {motion} from "framer-motion"
+import LoadingScreen from '../components/loadingscreen/LoadingScreen';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [showLoading, setShowLoading] = useState(true);
+  
+  if (!showLoading) {
+    return <Component {...pageProps} />
+  }
+  
+  return (
+    <LoadingScreen showLoading={showLoading} setShowLoading={setShowLoading} />
+  );
 }
