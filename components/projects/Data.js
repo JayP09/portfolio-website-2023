019404d796a3e7a4bfbf200df656ae5640214b78
Jay <panchalj272@gmail.com>
2023-01-10 23:20:03 +0530

    Feature: Updated Project Data

--- components/projects/Data.js
+++ components/projects/Data.js
@@ -1,41 +1,83 @@
-import ProjectImage from "../../public/twittercloneimage.png"
+import ProjectImage1 from "../../public/dustecosystem.jpg"
+import ProjectImage2 from "../../public/twittercloneimage.png"
+import ProjectImage3 from "../../public/nftmarketplace.png"
+import ProjectImage4 from "../../public/memegenerator.png"
+import ProjectImage5 from "../../public/TragicSimpbot.jpg"
+import ProjectImage6 from "../../public/JokeApp.jpg"
+import ProjectImage7 from "../../public/100DaysOfWeb3.jpg"
 
 export const projectsData = [
   {
     id: 1,
-    image: ProjectImage,
+    image: ProjectImage1,
     title: 'Dustecosystem',
-    category: 'App',
+    description: "Fully functional ecosystem website from scratch for an NFT project",
+    category: 'app',
+    link: "https://dustecosystem.xyz/",
+    githubLink: "https://github.com/DustEcosystem/dustecosystemwebsite"
   },
   {
     id: 2,
-    image: ProjectImage,
+    image: ProjectImage2,
     title: 'Bird App',
-    category: 'App',
+    description: "The Bird app is a Twitter clone created using React and Tailwind CSS.",
+    category: 'app',
+    link: "https://twitterclone-birdapp.vercel.app/",
+    githubLink: "https://github.com/JayP09/BirdApp"
   },
   {
     id: 3,
-    image: ProjectImage,
-    title: 'Joke App',
-    category: 'App',
+    image: ProjectImage3,
+    title: 'NFT MarketPlace Landing Page',
+    description: "NFT Marketplace frontend created using React and TailwindCSS",
+    category: 'design',
+    link: "https://nft-marketplace-jayp09.vercel.app/",
+    githubLink: "https://github.com/JayP09/NFT_Marketplace"
   },
   {
     id: 4,
-    image: ProjectImage,
-    title: 'Tragicsimp Bot',
-    category: 'Bot',
+    image: ProjectImage4,
+    title: 'Memegenerator',
+    description: "Memegenerator is a React app where the user gives text input on an image, and the app will generate a meme with one click for the user.",
+    category: 'app',
+    link: "https://memegenerator-phi.vercel.app/",
+    githubLink: "https://github.com/JayP09/Memegenerator"
   },
   {
     id: 5,
-    image: ProjectImage,
-    title: '100DaysofWeb3',
-    category: 'Bot',
+    image: ProjectImage5,
+    title: 'Tragicsimp Bot',
+    description: "TragicSimp is a discord bot that can send memes, facts, jokes, quotes, and track user stats.",
+    category: 'bot',
+    link: "https://github.com/JayP09/TragicSimpBot",
+    githubLink: "https://github.com/JayP09/TragicSimpBot"
+  },
+  {
+    id: 6,
+    image: ProjectImage6,
+    title: 'Joke App',
+    description: "The Joke App fetches the joke from the Joke API and shows it on screen with great UI.",
+    category: 'app',
+    link: "https://joke-app-psi.vercel.app/",
+    githubLink: "https://github.com/JayP09/JokeApp"
+  },
+  {
+    id: 7,
+    image: ProjectImage7,
+    title: '100DaysofWeb3 bot',
+    description: "Twitter bot for #100DaysOfWeb3",
+    category: 'bot',
+    link: "https://twitter.com/_100DaysOfWeb3",
+    githubLink: "https://github.com/JayP09/100DaysOfWeb3-twitter-bot"
   },
 ]
 
 export const projectNav = [
   {
     name: "all",
+  },
+  {
+    name: "design",
   },
   {
     name: "app",
--- components/projects/ProjectItems.js
+++ components/projects/ProjectItems.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import Image from 'next/image'
+import {motion} from "framer-motion"
+import { UilEye, UilGithub } from '@iconscout/react-unicons'
 
 
 const ProjectItems = ({project}) => {
   return (
     <div className='bg-containerColor border-[1px] border-solid border-[rgba(0,0,0,0.1)] p-4 lg:p-5 rounded-2xl' key={project.id}>
-      <div className="w-72 h-48 mb-3 lg:mb-4 flex justify-center items-center" >
-        <Image src={project.image} alt="project Image" className='rounded-2xl' />
+      <div className="w-72 h-48 mb-3 lg:mb-4 flex justify-center items-center relative" >
+        <Image src={project.image} alt={project.title} className='rounded-2xl object-cover' />
+        <motion.div
+          whileHover={{ opacity: [0, 1] }}
+          transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+          className="absolute flex justify-center items-center top-0 left-0 bottom-0 right-0 w-full h-full bg-titleColor/70 rounded-lg opacity-0 transition-[all_0.3s_ease] gap-3"
+        >
+          <a href={project.link} target="_blank" rel="noreferrer">
+            <motion.div
+              whileInView={{ scale: [0, 1] }}
+              whileHover={{ scale: [1, 0.90] }}
+              transition={{ duration: 0.25 }}
+              className="flex justify-center items-center text-titleColor bg-white m-4 rounded-full"
+            >
+              <UilEye className="h-10 w-10" />
+            </motion.div>
+          </a>
+          <a href={project.githubLink} target="_blank" rel="noreferrer">
+            <motion.div
+              whileInView={{ scale: [0, 1] }}
+              whileHover={{ scale: [1, 0.90] }}
+              transition={{ duration: 0.25 }}
+              className="flex justify-center items-center text-titleColor bg-white m-4 rounded-lg"
+            >
+              <i className='bx bxl-github text-4xl'></i>
+            </motion.div>
+          </a>
+        </motion.div>
       </div>
       
-      <h3 className="text-base font-medium mb-1 lg:mb-2">
+      <h3 className="text-base text-center font-medium mb-2 lg:mb-3">
         {project.title}
       </h3>
-      <a href='#' className="group text-textColor text-sm flex items-center gap-x-1">
-        Demo <i className='bx bx-right-arrow-alt text-base transition-[0.4s] group-hover:translate-x-1'></i>
-      </a>
+      <p className="group text-textColor text-sm flex items-center gap-x-1">
+        {project.description.slice(0,38)+"..."}
+      </p>
     </div>
   )
 }
